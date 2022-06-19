4bb22ee6b8c51ac4b2a621b499e632bd23ff5ec9
SkyfallWasTaken <55807755+SkyfallWasTaken@users.noreply.github.com>
2022-06-19 18:36:55 +0100

    Update and rename src/main/kotlin/net/fabricmc/example/ExampleMod.kt to src/main/kotlin/tk/skyfall/betterminehut/BetterMinehut.kt

---
+++ src/main/kotlin/tk/skyfall/betterminehut/BetterMinehut.kt
@@ -0,0 +1,10 @@
+package tk.skyfall.betterminehut
+import net.fabricmc.api.ModInitializer
+
+@Suppress("UNUSED")
+object ModName: ModInitializer {
+    private const val MOD_ID = "better_minehut"
+    override fun onInitialize() {
+        println("Example mod has been initialized.")
+    }
+}
